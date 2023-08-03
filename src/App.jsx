315cfe238d51d7fb451626fc16b20b8df9272c51
elsusu11 <toxicxvictor@gmail.com>
2023-08-03 16:12:06 +0200

    Se agrega el Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt="El avatar del suave" src="https://d131oejryywhj7.cloudfront.net/p/api/usuario/dup/4BWneqsF2kiAaQmZ8JWlQ40gKwJIa5oU0.jpg/300x300cut/?s=l" />
+                    <div>
+                        <strong>Víctor Suesta Arribas</strong>
+                        <span>@v.suestx</span>
+                    </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
